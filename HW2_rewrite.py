c1d9705ee465dfdb531e9d722a5afb65a821a201
irseppi <irseppi@github.com>
2026-01-31 11:50:22 -0900

    Update

--- HW2_rewrite.py
+++ HW2_rewrite.py
@@ -18,10 +18,8 @@ def get_f(v0,l,c,t_array,f0):
         ft.append(ft0p)
     return ft
 
-tpr     = np.arange(0, 241, 1)
-
 varrying_variables = {
-    'base': np.array([]),  # Placeholder for base case
+    'base': np.arange(0, 241, 1),  # Placeholder for time array
     'v0':  np.arange(0, 200, 20),
     'l':   np.arange(0, 5000, 200),
     'c':   np.arange(200, 500, 5),
@@ -29,17 +27,19 @@ varrying_variables = {
     'f0':  np.arange(0, 250, 20)
 }
 
-cm = plt.cm.rainbow
+cm = plt.cm.rainbow #set colormap
 
 fig, axs = plt.subplots(2, 3, figsize=(14, 10))
 axs      = axs.flatten()
 
 for n, (var_name, var_values) in enumerate(varrying_variables.items()):
-    f0      = 150
-    tprime0 = 120 
-    c       =  320
-    v0      = 80
-    l       = 4000
+
+    f0      = 150 # Default frequancy of aircraft source (Hz)
+    tprime0 = 120 # Default time .... (s)
+    c       =  320 # Default speed of sound (m/s)
+    v0      = 80 # Default speed of aircraft (m/s)
+    l       = 4000 # Default closest approach between aircraft and sensor (m)
+    tpr     = varrying_variables['base'] # Time array at sensor (s)
 
     tprime  = get_t(v0,l,c,tprime0,tpr)
     ft      = get_f(v0, l, c, tprime, f0)
@@ -51,9 +51,9 @@ for n, (var_name, var_values) in enumerate(varrying_variables.items()):
     axs[n].set_ylim(50, 250)
     axs[n].set_xlim(0, 240)
 
-    if n == 0 or n == 3:
+    if n in [0, 3]:
         axs[n].set_ylabel('Frequency (Hz)')
-    if n == 3 or n == 4 or n == 5:
+    if n in [3, 4, 5]:
         axs[n].set_xlabel('Time (s)')
 
     if n == 0:
@@ -67,18 +67,18 @@ for n, (var_name, var_values) in enumerate(varrying_variables.items()):
 
     for val in var_values:
         if n == 1:
-            v0 = val
+            v0      = val
         elif n == 2:
-            l = val
+            l       = val
         elif n == 3:
-            c = val
+            c       = val
         elif n == 4:
             tprime0 = val
         elif n == 5:
-            f0 = val
+            f0      = val
         
         tprime = get_t(v0, l, c, tprime0, tpr)
-        ft = get_f(v0, l, c, tprime, f0)
+        ft     = get_f(v0, l, c, tprime, f0)
 
         axs[n].plot(tpr, ft, color=cm(norm(val)), linewidth=0.5)
 
