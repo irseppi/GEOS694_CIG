f18c0df9c4717c1236d82300284c9f88e58ed3bb
irseppi <irseppi@github.com>
2026-02-19 16:44:08 -0900

    Lab5

---
+++ Lab5/2d_gaussian.py
@@ -0,0 +1,43 @@
+import time
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+STEP = 0.001
+
+
+def gaussian_2d(x, y, sigma):
+    """Calculate 2D Gaussian value at (x, y)."""
+    return (1 / (2 * np.pi * sigma**2)) * np.exp(
+        -1 * (x**2 + y**2) / (2 * sigma**2)
+    )
+
+
+def plot(z):
+    """Plot 2D Gaussian data."""
+    plt.imshow(z.T)
+    plt.gca().invert_yaxis()
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.title(f"{z.shape} points")
+    plt.gca().set_aspect(1)
+
+
+def main(xmin, xmax, ymin, ymax, sigma=1):
+    """Generate and plot 2D Gaussian."""
+    x = np.arange(float(xmin), float(xmax), STEP)
+    y = np.arange(float(ymin), float(ymax), STEP)
+    z = []
+    for x_val in x:
+        for y_val in y:
+            z.append(gaussian_2d(x_val, y_val, sigma))
+    zz = np.array(z).reshape(len(x), len(y))
+    plot(zz)
+
+
+if __name__ == "__main__":
+    start = time.time()
+    main(-2, 2, -2, 2)
+    elapsed = time.time() - start
+    print(f"Elapsed Time: {elapsed}s")
+    plt.show()
